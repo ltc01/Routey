61bcca42b91eb6f93103aa6b0c62a64007670078
ltc01 <laxmichoudhary909@gmail.com>
2024-02-06 16:32:40 +0530

    added section

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Service = () => {
   return (
+    <>
     <div>Services provided</div>
+    <div>
+      <h1>Serivice</h1>
+      <p>detail</p>
+    </div>
+    </>
   )
 }
 
